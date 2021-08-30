e69a71f3851ecbc1601d49300a7788bde57b52da
Marharyta Bosak <aboimova.rita@gmail.com>
2021-08-30 23:46:25 +0300

    task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,15 @@
-const categories = document.querySelectorAll('.item')
-console.log(`В списке ${categories.length} категории.`);
+const categoriesList = document.querySelectorAll('.item')
+console.log(`В списке ${categoriesList.length} категории.`);
+
+const findNumberOfCategoriesElements = categoriesList.forEach((categorie) => { 
+
+    const categorieName = categorie.firstElementChild;
+
+    const categorieElementsList = categorie.lastElementChild;
+
+    return console.log(
+    `
+    - Категория: ${categorieName.innerHTML} 
+    - Количество элементов: ${categorieElementsList.childElementCount}`)
+})
 
