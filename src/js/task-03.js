b7299cd3a3b1c6e3e3e5253ddedd01b37b880f49
Marharyta Bosak <aboimova.rita@gmail.com>
2021-09-22 23:40:08 +0300

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -14,7 +14,7 @@ const images = [
 ];
 
 const markupGallery = images.map((image) =>
-{ return `<li><img class = "image-gallery"; src = ${image.url}; alt = ${image.alt}/></li>`;}).join('');
+{ return `<li class = "item-gallery"><img class = "image-gallery"; src = ${image.url}; alt = ${image.alt}/></li>`;}).join('');
 
 console.log(markupGallery)
 
@@ -22,3 +22,38 @@ const listGallery = document.querySelector('#gallery');
 
 listGallery.insertAdjacentHTML("afterbegin", markupGallery);
 
+const itemGallery = document.querySelectorAll('.item-gallery');
+const imageGallery = document.querySelectorAll('.image-gallery');
+
+listGallery.style.margin = "0 auto"; 
+listGallery.style.maxWidth = "1050px";
+listGallery.style.height = "430px";
+listGallery.style.padding = "0 20px";
+listGallery.style.display = "grid";
+listGallery.style.justifyItems = 'center';
+listGallery.style.alignItems = 'center';
+// listGallery.style.gridTemplateColumns = "repeat(5, 140px);";
+// listGallery.style.gridTemplateRows = "repeat(9, 43px);";
+// listGallery.style.columnGap = '5px';
+// listGallery.style.rowGap = '5px';
+
+
+imageGallery.style.display = 'block';
+imageGallery.style.maxWidth = '100%';
+imageGallery.style.height = 'auto';
+imageGallery.style.width = '200px';
+
+
+// {
+//   margin: 0 auto;
+//   max-width: 1050px;
+//   height: 430px;
+//   padding-left: 20px;
+//   padding-right: 20px;
+//   background-color: white;
+//   display: grid;
+//   grid-template-columns: repeat(5, 140px);
+//   grid-template-rows: repeat(9, 43px);
+//   row-gap: 5px;
+//   column-gap: 5px;
+// }
