ed007b45fecca7d906dd680f1f4cbd54e7c3652c
Marharyta Bosak <aboimova.rita@gmail.com>
2021-10-08 22:26:41 +0300

    task-5: done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -4,16 +4,14 @@ const btnIncrement = document.querySelector('button[data-action = "increment"]')
 const btnDecrement = document.querySelector('button[data-action = "decrement"]')
 
 const handleClickIncrement = () => {
-  const counterValueNumber = Number(counterValue.innerHTML);
-  return counterValue.innerHTML = counterValueNumber + 1;
+    const counterValueNumber = Number(counterValue.innerHTML);
+    return counterValue.innerHTML = counterValueNumber + 1;
 };
 
 const handleClickDecrement = () => {
     const counterValueNumber = Number(counterValue.innerHTML);
-  return counterValue.innerHTML = counterValueNumber - 1;
+    return counterValue.innerHTML = counterValueNumber - 1;
 };
 
 btnIncrement.addEventListener('click', handleClickIncrement)
 btnDecrement.addEventListener('click', handleClickDecrement)
-
-console.log(counterValue);
---
+++ src/js/task-05.js
@@ -0,0 +1,15 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+const handleNameOutput = () => {
+
+   if (nameInput.value.length === 0){
+    return nameOutput.innerHTML = 'незнакомец';
+    };
+
+    return nameOutput.innerHTML = nameInput.value;
+};
+
+nameInput.addEventListener('input', handleNameOutput);
+
+console.log(nameInput.value);
