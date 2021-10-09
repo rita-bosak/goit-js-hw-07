7ea4ce11e9f0ac6a23c176b6a4004f684dbb638e
Marharyta Bosak <aboimova.rita@gmail.com>
2021-10-10 00:27:59 +0300

    task 8

---
+++ src/js/task-08.js
@@ -0,0 +1,42 @@
+const inputNumber = document.querySelector('input');
+const btnRender = document.querySelector('button[data-action = "render"]');
+const btnDestroy = document.querySelector('button[data-action = "destroy"]');
+const divBoxes = document.querySelector('#boxes');
+const style = document.querySelector('style');
+
+function createBoxes(amount) {
+
+    for (let i = 1; i <= amount; i += 1){
+
+        const collectionEl = document.createElement('div');
+        
+        divBoxes.appendChild(collectionEl);
+
+        collectionEl.classList.add(`box-${[i]}`);
+        
+        const boxStyle = 
+        `.box-${[i]}{
+            background-color: rgb(${(Math.random() * 255) + 1}, ${(Math.random() * 255) + 1}, ${(Math.random() * 255) + 1});
+            width: ${[i] * 10 + 20}px;
+            height: ${[i] * 10 + 20}px;
+        }`;
+
+        style.insertAdjacentHTML('beforeend', boxStyle);
+    };
+    
+    return divBoxes;
+};
+
+const handleBoxesRender = () => {
+    const amount = Number(inputNumber.value);
+    return createBoxes(amount);
+
+};
+
+const handleDestroyBoxes = () => {
+function destroyBoxes() {
+    return divBoxes.innerHTML = '';
+}
+};
+btnRender.addEventListener('click', handleBoxesRender);
+btnDestroy.addEventListener('click', handleDestroyBoxes);
